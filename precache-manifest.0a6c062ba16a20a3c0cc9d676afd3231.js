addb3c77d545309254ce0ef024d7224aea25ab7a
Fu Hui <evanhui0310@gmail.com>
2020-07-28 03:30:38 -0700

    Updates

---
+++ precache-manifest.0a6c062ba16a20a3c0cc9d676afd3231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e46b4c014c34fa9d43be63def3793c0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "73e23c0ee8e911f596a3",
+    "url": "/monster-rolodex/static/css/main.f6c26626.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e23c0ee8e911f596a3",
+    "url": "/monster-rolodex/static/js/main.58426066.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
